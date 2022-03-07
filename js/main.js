f95c88418db8d415c569992d3d792605d604fc53
marcelojoc <marcelojoc@gmail.com>
2022-03-07 09:44:16 -0300

    ejemplo inmutabilidad

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+// con Jquery
+// let description = $('#description')
+// let calories = $('#calories')
+// let carbs = $('#carbs')
+// let protein = $('#protein')
+
+
+// VARIABLES DE LOS INPUTS
+let Description = document.getElementById('Descripcion');
+let Calories = document.getElementById('Calorias'); 
+let Carbs = document.getElementById('Carbs');
+let Proteinas = document.getElementById('Protein');
+
+// VALORES DE LOS INPUTS
+Description.value;
+Calories.value;
+Carbs.value;
+Proteinas.value;
